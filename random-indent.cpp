34f49d732acbb38b4225b0c923797681d25f72e1
Rebecca Turner <637275@gmail.com>
2016-06-29 14:52:35 -0400

    fixed the goddamn .gitignore, which was in utf16le on accident

--- random-indent.cpp
+++ random-indent.cpp
@@ -5,12 +5,12 @@
 #include <streambuf>
 
 int main(int argc, const char** argv) {
-	std::ifstream t("Makefile");
-	t.seekg(0, std::ios::end);
-	size_t size = t.tellg();
+	std::ifstream infile("Makefile");
+	infile.seekg(0, std::ios::end);
+	size_t size = infile.tellg();
 	std::string buffer(size, ' ');
-	t.seekg(0);
-	t.read(&buffer[0], size);
+	infile.seekg(0);
+	infile.read(&buffer[0], size);
 	std::cout << buffer << "\n\r";
 	return 0;
 }
